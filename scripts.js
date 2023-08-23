0dd68653fe6baecef8a99a64c13e28055ca18d66
fjdsal <118019405+fjdsal@users.noreply.github.com>
2023-08-23 20:48:03 +0000

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,13 +3,13 @@
     var swiper = new Swiper('#swiper-container', { // swiper ist das wenn die bilder groß werden und man wischen kann, hier verschiedene optionen
       slidesPerView: 1,
       spaceBetween: 10,
-      loop: false,
+      loop: false,   // loops (das wenn man am letzten foto ist und nach rechts wischt, man wieder beim ersten bild ist) funktionieren leider nicht gut, dann öffnet es nicht immer das bild was man angeklickt hat
       centeredSlides: true,
       slidesOffsetBefore: 0,
       slidesOffsetAfter: 0,
       keyboard: { enabled: true, onlyInViewport: true },   
       mousewheel: { invert: false },
-      pagination: {
+      pagination: { // die Punkte unter dem bild
         el: '.swiper-pagination',
         clickable: true,
       },
@@ -20,12 +20,12 @@
       
 
     });
-    if (window.innerWidth <= 1100) {
+    if (window.innerWidth <= 1100) { // damit man am handy nicht zoomen kann
       let metaViewport = document.querySelector('meta[name=viewport]');
       metaViewport.content = 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no';
     }
         
-    var thumbnailRect;
+    var thumbnailRect; // merkt sich position vom thumbnail für die Schließungsanimation
     
     function openGallery(event, gallery, startSlide) {
       event.preventDefault();
@@ -35,21 +35,21 @@
     
       const swiperContainer = document.getElementById('swiper-container');
     
-      // Set the initial position and size of the swiper container to match the thumbnail
+      // anfangsposition und größe vom thumbnail
       swiperContainer.style.position = 'fixed';
       swiperContainer.style.top = `${thumbnailRect.top}px`;
       swiperContainer.style.left = `${thumbnailRect.left}px`;
       swiperContainer.style.width = `${thumbnailRect.width}px`;
       swiperContainer.style.height = `${thumbnailRect.height}px`;
     
-      // Show the swiper container without animation
+      //zuerst swiper ohne animation zeigen
       swiperContainer.style.transition = 'none';
       swiperContainer.classList.add('open');
     
-      // Trigger a reflow to apply the initial transformation before animating
+     
       void swiperContainer.offsetWidth;
     
-      // Restore the transition and animate to the normal position and size
+      
       swiperContainer.style.transition = 'all 0.4s ease-in-out';
       swiperContainer.style.top = '0';
       swiperContainer.style.left = '0';
@@ -58,12 +58,13 @@
     
       document.body.classList.add('blur');
       document.body.style.overflow = 'hidden';
+        
       if (window.innerWidth <= 1100) {
-        document.body.style.position = 'fixed';
+        document.body.style.position = 'fixed';  // bei safari am handy kann man den hintergrund noch scrollen, hier ein workaround damit man nur nach links und rechts wischen kann
       }
     
       swiper.removeAllSlides();
-    
+      // fügt alle bilder hinzu
       for (var i = 0; i < gallery.length; i++) {
         var slide = document.createElement('div');
         slide.className = 'swiper-slide';
@@ -74,21 +75,21 @@
       }
     
       if (startSlide) {
-        swiper.slideTo(startSlide, 0);
+        swiper.slideTo(startSlide, 0);   // wenn kein spezifisches bild zum öffnen angegeben ist, öffnet es das erste
       }
     }
     
     function closeGallery() {
       const swiperContainer = document.getElementById('swiper-container');
     
-      // Function to handle the transition end and cleanup
-      function onTransitionEnd() {
+    
+      function onTransitionEnd() {   // es wird wieder alles css zurückgesetzt
         swiperContainer.classList.remove('open');
         document.body.classList.remove('blur');
         document.body.style.overflow = '';
         document.body.style.position = '';
     
-        // Reset styles
+        
         swiperContainer.style.transition = '';
         swiperContainer.style.top = '';
         swiperContainer.style.left = '';
@@ -98,20 +99,20 @@
         swiperContainer.removeEventListener('transitionend', onTransitionEnd);
       }
     
-      // Set the final thumbnail's dimensions and position
+      // swiper verkleinert sich zu originaler thumbnail position
       swiperContainer.style.top = `${thumbnailRect.top}px`;
       swiperContainer.style.left = `${thumbnailRect.left}px`;
       swiperContainer.style.width = `${thumbnailRect.width}px`;
       swiperContainer.style.height = `${thumbnailRect.height}px`;
     
-      // Add a transition end event listener
+      // warten bis verkleinerung fertig
       swiperContainer.addEventListener('transitionend', onTransitionEnd);
     
-      // Start the animation
+      // verschwommenes ausmachen animation
       swiperContainer.style.transition = 'all 0.4s ease-in-out';
     }
     
-    document.addEventListener('keydown', function (event) {
+    document.addEventListener('keydown', function (event) { //escape zum schließen möglich
       if (event.key === 'Escape') {
         closeGallery();
       }
